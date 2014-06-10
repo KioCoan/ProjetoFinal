2a1cb04a468038f0bee85417ec0b9b3f22f9b250
Rafael <rafaellfreitas@gmail.com>
2014-06-10 17:21:58 -0300

    Spritekit + Story board rulador

--- ProjetoFinal/ViewController.m
+++ ProjetoFinal/ViewController.m
@@ -18,6 +18,33 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self setMeuNo:[[SKSpriteNode alloc]init] ];
+    //[self.view addSubview:self.meuNo];
+    SKScene *cena = [[SKScene alloc]initWithSize:CGSizeMake(self.minhaCena.frame.size.width, self.minhaCena.frame.size.height)];
+    [cena setPosition:self.minhaCena.center];
+    [cena setBackgroundColor:[UIColor whiteColor]];
+    SKTexture *textura = [SKTexture textureWithImageNamed:@"gParada.png"];
+    [self.meuNo setTexture:textura];
+    
+    [self.meuNo setSize:CGSizeMake(50, 50)];
+    [self.meuNo setPosition:CGPointMake(cena.size.width / 2, cena.size.height / 2)];
+    
+    //[cena addChild:self.meuNo];
+    
+    SKLabelNode *label = [[SKLabelNode alloc] init];
+    [label setText:@"Testando SKLabelNode"];
+    [label setPosition:CGPointMake(cena.size.width / 2, cena.size.height / 2)];
+    
+    
+    [label setFontColor:[UIColor blackColor]];
+    [cena addChild:label];
+    
+    [self.minhaCena presentScene:cena];
+    
+    
+    self.minhaLabel.text = label.text;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +53,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)mudarLabel:(id)sender {
+    [self.minhaCena presentScene:nil];
+}
 @end
