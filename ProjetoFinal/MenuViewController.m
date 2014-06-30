a097f0051f8922e0b65fae60db193a844b3dafd5
felipeCostaNascimento <fcnfelipe@gmail.com>
2014-06-30 16:29:45 -0300

    beta do menu programaticamente criado

--- ProjetoFinal/MenuViewController.m
+++ ProjetoFinal/MenuViewController.m
@@ -12,6 +12,9 @@
 
 @end
 
+static int BOTAO_WIDTH = 165;
+static int BOTAO_HEIGHT = 113;
+
 @implementation MenuViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -27,6 +30,56 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    CGRect frame = self.view.frame;
+    frame.origin.y = 64;
+    frame.size.height = self.view.frame.size.height - frame.origin.y;
+    
+    UIScrollView *scroll = [[UIScrollView alloc] initWithFrame:frame];
+    [scroll setBackgroundColor:[UIColor grayColor]];
+    [scroll setUserInteractionEnabled:YES];
+    [scroll setScrollEnabled:YES];
+    
+    
+    [[self view] addSubview:scroll];
+    
+    int valorY = 1;
+    
+    for(int i=0; i<7; i++){
+        //INICIALIZA VIEW DE FUNDO
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(scroll.frame.origin.x, valorY, scroll.frame.size.width, 200)];
+        [view setBackgroundColor:[UIColor whiteColor]];
+        [scroll addSubview:view];
+        
+        //INICIALIZA OS NUMEROSDA DIREITA
+        UILabel *numero = [[UILabel alloc] initWithFrame:CGRectMake(view.frame.origin.x + 10, 93, 10, 14)];
+        [numero setText:[NSString stringWithFormat:@"%d", (i + 1)]];
+        [numero setTextColor:[UIColor blackColor]];
+        [view addSubview:numero];
+        
+        //POSIÇOES DO BOTAO
+        int posicaoXBotao = (view.frame.size.width / 2) - (BOTAO_WIDTH / 2);
+        int posicaoYBotao = (view.frame.size.height / 2) - (BOTAO_HEIGHT / 2);
+        
+        //INICIALIZA BOTAO
+        UIButton *botao = [[UIButton alloc] initWithFrame:CGRectMake(posicaoXBotao, posicaoYBotao, BOTAO_WIDTH, BOTAO_HEIGHT)];
+        UIImage *imgCaixa = [UIImage imageNamed:@"item-variavel3.png"];
+        [botao setImage:imgCaixa forState:UIControlStateNormal];
+        [botao setTag:1];
+        [botao setTitle:@"BTN" forState:UIControlStateNormal];
+        [botao addTarget:self action:@selector(mostrarAssunto:) forControlEvents:UIControlEventTouchUpInside];
+        [view addSubview:botao];
+        
+        //DEFINE NOVA POSIÇAO Y PARA A PROXIMA VIEW
+        valorY += view.frame.size.height + 1;
+    }
+    
+    [scroll setContentSize:CGSizeMake(scroll.frame.size.width, valorY)];
+}
+
+
+-(void)mostrarAssunto:(id)sender{
+    NSLog(@"Clicou %@", [sender currentTitle]);
 }
 
 - (void)didReceiveMemoryWarning
--- ProjetoFinal/ViewController.m
+++ ProjetoFinal/ViewController.m
@@ -12,13 +12,18 @@
 
 @end
 
+
+
 @implementation ViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+   
 }
+
 
 - (void)didReceiveMemoryWarning
 {
