5d34be31dd67701cf901876b19f29b532b38fa08
felipeCostaNascimento <fcnfelipe@gmail.com>
2014-08-05 17:41:38 -0300

    novos sprites condicionais iniciado

---
+++ ProjetoFinal/TipoCondicionalNode.m
@@ -0,0 +1,13 @@
+//
+//  TipoCondicionalNode.m
+//  ProjetoFinal
+//
+//  Created by Felipe Costa Nascimento on 05/08/14.
+//  Copyright (c) 2014 SENAC - iOS. All rights reserved.
+//
+
+#import "TipoCondicionalNode.h"
+
+@implementation TipoCondicionalNode
+
+@end
