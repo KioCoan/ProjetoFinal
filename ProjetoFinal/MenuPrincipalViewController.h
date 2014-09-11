2906bdda511097d164912e586a325be08800b713
Rafael Freitas <rafaellfreitas@gmail.com>
2014-09-11 15:41:10 -0300

    Ajustes StoryBoard

---
+++ ProjetoFinal/MenuPrincipalViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuPrincipalViewController.h
+//  ProjetoFinal
+//
+//  Created by Rafael Aparecido de Freitas on 11/09/14.
+//  Copyright (c) 2014 SENAC - iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuPrincipalViewController : UIViewController
+
+@end
---
+++ ProjetoFinal/MenuPrincipalViewController.m
@@ -0,0 +1,49 @@
+//
+//  MenuPrincipalViewController.m
+//  ProjetoFinal
+//
+//  Created by Rafael Aparecido de Freitas on 11/09/14.
+//  Copyright (c) 2014 SENAC - iOS. All rights reserved.
+//
+
+#import "MenuPrincipalViewController.h"
+
+@interface MenuPrincipalViewController ()
+
+@end
+
+@implementation MenuPrincipalViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
