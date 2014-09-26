18b76376cc6bef9029148c469be3dbb6642454c5
Rafael Freitas <rafaellfreitas@gmail.com>
2014-09-26 11:46:17 -0300

    Ai ai ai

--- ProjetoFinal/Tools/Desafios/DesafioOperadores.m
+++ ProjetoFinal/Tools/Desafios/DesafioOperadores.m
@@ -1,1 +1,144 @@
+//
+//  Desafio.m
+//  ProjetoFinal
+//
+//  Created by Rafael Aparecido de Freitas on 15/09/14.
+//  Copyright (c) 2014 SENAC - iOS. All rights reserved.
+//
+
+#import "DesafioOperadores.h"
+#import "Expressao.h"
+@implementation DesafioOperadores
+
+
+-(id)initWithLevel:(int)level andType:(NSString *)type andTasks:(int)tasks{
+    self = [super init];
+    if (self) {
+        listaTarefas = [[NSMutableArray alloc] init];
+        acertos = [[NSMutableArray alloc] init];
+        erros = [[NSMutableArray alloc] init];
+        [self montaDesafioNivel:level tipo:type nTarefas:tasks];
+    }
+    return self;
+}
+
+-(id)init{
+    self = [super init];
+    if (self) {
+        listaTarefas = [[NSMutableArray alloc] init];
+        desafioConcluido = NO;
+    }
+    return self;
+}
+-(void)montaDesafioNivel:(int)level tipo:(NSString*)type nTarefas:(int)nTarefas{
+    nivel = level;
+    tipo = type;
+    nTasks = nTarefas;
+    tarefaAtual = 0;
+}
+
+-(int)nTarefas{
+    return nTasks;
+}
+
+-(NSString*)parte1{
+    return [[listaTarefas objectAtIndex:tarefaAtual]saidaParte1];
+}
+-(NSString*)operador{
+    return [[listaTarefas objectAtIndex:tarefaAtual]operador];
+}
+-(NSString*)parte2{
+    return [[listaTarefas objectAtIndex:tarefaAtual]saidaParte2];
+}
+-(NSString*)resultado{
+    return [[listaTarefas objectAtIndex:tarefaAtual]resultado];
+}
+-(BOOL)incrementaTarefa{
+    tarefaAtual++;
+    if (tarefaAtual >= nTasks) {
+        tarefaAtual--;
+        [self finalizaDesafio];
+        return NO;
+    }else{
+        
+        return YES;
+    }
+}
+-(BOOL)decrementaTarefa{
+    tarefaAtual--;
+    if (tarefaAtual <= 0) {
+        return NO;
+    }else{
+        return YES;
+    }
+}
+-(NSMutableArray*)retornaTarefas{
+    return listaTarefas;
+}
+-(void)instanciaTarefas{
+    for (int i = 0; i<nTasks; i++) {
+        [listaTarefas addObject:[[Expressao alloc] initWithNivel:nivel andOperator:tipo]];
+    }
+}
+-(BOOL)respostaDupla{
+    
+    return [[listaTarefas objectAtIndex:tarefaAtual]respostaDupla];
+}
+-(BOOL)corrige:(NSString*)opcao{
+    if ([self respostaDupla] || [[self operador]isEqualToString:opcao]) {
+        //[self acertou:YES];
+        nAcertos++;
+        return YES;
+    }else{
+        nErros++;
+        return NO;
+    }
+}
+
+//-(void)acertou:(BOOL)acertou{
+//    if (acertou) {
+//        for (int i=0;i<acertos.count; i++) {
+//            if ([acertos objectAtIndex:i] == [NSNumber numberWithInt:tarefaAtual]) {
+//                NSLog(@"Passou");
 //                return;
+//            }
+//        }
+//        [acertos addObject:[NSNumber numberWithInt: tarefaAtual]];
+//    }else{
+//        for (int i=0;i<erros.count; i++) {
+//            if ([erros objectAtIndex:i] == [NSNumber numberWithInt:tarefaAtual]) {
+//                NSLog(@"Passou");
+//                return;
+//            }
+//        }
+//        [erros addObject:[NSNumber numberWithInt: tarefaAtual]];
+//    }
+//}
+
+
+-(void)finalizaDesafio{
+    desafioConcluido = YES;
+//    nAcertos = 0;
+//    nErros = 0;
+}
+-(BOOL)desafioFinalizado{
+    return desafioConcluido;
+}
+-(void)restart{
+    tarefaAtual = 0;
+    desafioConcluido = NO;
+    nAcertos = 0;
+    nErros = 0;
+//    [acertos removeAllObjects];
+//    [erros removeAllObjects];
+}
+-(int)acertos{
+    return nAcertos;
+}
+-(int)erros{
+    return nErros;
+}
+-(int)retornaTarefaAtual{
+    return tarefaAtual;
+}
+@end
